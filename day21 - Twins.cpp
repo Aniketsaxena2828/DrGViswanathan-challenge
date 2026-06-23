59577aabf6547f28cf7cf075ff3871514884ed6d
Aniket <aniket07jan@gmail.com>
2026-06-24 04:59:46 +0530

    day21

---
+++ day21 - Twins.cpp
@@ -0,0 +1,34 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int n;
+    cin >> n;
+ 
+    vector<int> coins(n);
+    int total = 0;
+ 
+    for (int i = 0; i < n; i++) {
+        cin >> coins[i];
+        total += coins[i];
+    }
+ 
+    sort(coins.begin(), coins.end(), greater<int>());
+ 
+    int mySum = 0;
+    int count = 0;
+ 
+    for (int coin : coins) {
+        mySum += coin;
+        count++;
+ 
+        if (mySum > total - mySum) {
+            break;
+        }
+    }
+ 
+    cout << count << endl;
+ 
+    return 0;
+}
